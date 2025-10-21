e7f24e010bc3c8457a3060f99bbd55f533e15d03
Philip Mumo <philipp1mogul@gmail.com>
2025-10-22 01:01:58 +0300

    Logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -59,7 +59,7 @@ const Header = ({ t }: { t: TFunction }) => {
       <Container>
         <Row justify="space-between">
           <LogoContainer to="/" aria-label="homepage">
-            <SvgIcon src="logo.svg" width="101px" height="64px" />
+            NESTONET
           </LogoContainer>
           <NotHidden>
             <MenuItem />
